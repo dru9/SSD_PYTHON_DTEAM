3103eccbe2eed5a426ed467a4bf641504a24a57f
dayoung <dayoung@hanmail.net>
2025-07-14 16:22:00 +0900

    [FEAT] Test codes for shell full read and write

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -1,4 +1,3 @@
-import pytest
 from pytest_mock import MockerFixture
 
 
@@ -19,3 +18,22 @@ def test_shell_write(mocker: MockerFixture):
     )
     assert res == "[Write] Done"
     shell.write.assert_called()
+
+
+def test_full_read(mocker: MockerFixture):
+    shell = mocker.Mock()
+    ret = "[Full Read]"
+    for i in range(100):
+        ret += f"\nLBA {i:0>2} : 0x00000000"
+    shell.full_read.return_value = ret
+    res = shell.full_read(lba=0)
+    assert res == ret
+    shell.full_read.assert_called()
+
+
+def test_full_write(mocker: MockerFixture):
+    shell = mocker.Mock()
+    shell.full_write.return_value = "[Full Write] Done"
+    res = shell.full_write(value="0x00000000")
+    assert res == "[Full Write] Done"
+    shell.full_write.assert_called()
