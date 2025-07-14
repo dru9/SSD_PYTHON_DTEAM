b19e3dc2f6e6d19d52f11e185c322097afc9dcb6
heejun-k <heejun99@gmail.com>
2025-07-14 14:40:24 +0900

    Add SSD class and __init__ function, and change 2nd parameter as string type

---
+++ ssd.py
@@ -0,0 +1,16 @@
+
+import os
+
+FILE_PATH = "ssd_output.txt"
+class SSD:
+    def __init__(self):
+        if not os.path.exists(FILE_PATH):
+            with open(FILE_PATH, "w") as f:
+                for i in range(100):
+                    f.write(f"{i}\t0x00000000\n")
+
+    def read(self, LBA):
+        pass
+
+    def write(self, LBA, data):
+        pass
--- tests/test_ssd.py
+++ tests/test_ssd.py
@@ -4,15 +4,15 @@ from ssd import SSD
 def test_write_ssd():
     ssd = SSD()
     loc = 3
-    ssd.write(loc, 0xFFFFFFFF)
+    ssd.write(loc, "0xFFFFFFFF")
 
     result = ssd.read(loc)
-    assert result == 0xFFFFFFFF
+    assert result == "0xFFFFFFFF"
 
 def test_write_check_file():
     ssd = SSD()
     loc = 11
-    ssd.write(loc, 0x1298CDEF)
+    ssd.write(loc, "0x1298CDEF")
 
     with open("ssd_output.txt", "r") as f:
         content = f.read()
@@ -21,7 +21,7 @@ def test_write_check_file():
 def test_out_of_index_error():
     ssd = SSD()
     loc = 120
-    ssd.write(loc, 0x1298CDEF)
+    ssd.write(loc, "0x1298CDEF")
 
     with open("ssd_output.txt", "r") as f:
         content = f.read()
