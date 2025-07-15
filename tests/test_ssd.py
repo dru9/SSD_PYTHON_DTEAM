fcf78748581c2cda3026d49eaaf0eec4527d698c
jeonghun <jhe.kim@samsung.com>
2025-07-15 14:19:33 +0900

    [REFACTOR] mock 객체를 공통으로 가져다 쓸 수 있도록 변경. SSD init에서 nand_file 만드는 부분을 FileManager로 변경.

--- tests/test_ssd.py
+++ tests/test_ssd.py
@@ -4,75 +4,64 @@ from pytest_mock import MockerFixture
 from ssd import SSD, FileManager
 
 
-def test_write_and_read(mocker: MockerFixture):
-    mock_file_manager = mocker.Mock()
-    ssd = SSD(mock_file_manager)
+@pytest.fixture
+def ssd(mocker):
+    file_manager = mocker.Mock()
+    ssd = SSD(file_manager)
+    return ssd
 
+
+def test_write_and_read(ssd):
     lba = 0
     expected = "0x12345678"
     ssd.write(lba, expected)
     ssd.read(lba)
 
-    mock_file_manager.read_nand_txt.assert_called_once()
-    mock_file_manager.write_nand_txt.assert_called_once()
-    mock_file_manager.write_output_txt.assert_called()
+    ssd.file_manager.read_nand_txt.assert_called_once()
+    ssd.file_manager.write_nand_txt.assert_called_once()
+    ssd.file_manager.write_output_txt.assert_called()
 
-def test_read_ssd_nand_txt_file_called_by_read(mocker: MockerFixture):
-    mock_file_manager = mocker.Mock()
-    ssd = SSD(mock_file_manager)
 
+def test_read_ssd_nand_txt_file_called_by_read(ssd):
     read_idx = 0
     ssd.read(read_idx)
 
-    mock_file_manager.read_nand_txt.assert_called_once()
-    mock_file_manager.write_output_txt.assert_called_once()
+    ssd.file_manager.read_nand_txt.assert_called_once()
+    ssd.file_manager.write_output_txt.assert_called_once()
 
 
-def test_read_ssd_nand_txt_file_called_by_write(mocker: MockerFixture):
-    mock_file_manager = mocker.Mock(spec=FileManager)
-    ssd = SSD(mock_file_manager)
-
+def test_read_ssd_nand_txt_file_called_by_write(ssd):
     write_idx = 0
     write_contents = "0x00001111"
     ssd.write(write_idx, write_contents)
 
-    mock_file_manager.write_nand_txt.assert_called()
+    ssd.file_manager.write_nand_txt.assert_called()
 
 
-def test_read_method_record_ssd_output_txt(mocker: MockerFixture):
-    mock_file_manager = mocker.Mock(spec=FileManager)
-    ssd = SSD(mock_file_manager)
-
+def test_read_method_record_ssd_output_txt(ssd):
     read_idx = 0
     ssd.read(read_idx)
 
-    mock_file_manager.read_nand_txt.assert_called()
-    mock_file_manager.write_output_txt.assert_called()
+    ssd.file_manager.read_nand_txt.assert_called()
+    ssd.file_manager.write_output_txt.assert_called()
 
 
-def test_write_ssd(mocker: MockerFixture):
-    mock_file_manager = mocker.Mock(spec=FileManager)
-    ssd = SSD(mock_file_manager)
+def test_write_ssd(ssd):
     loc = 3
     ssd.write(loc, "0xFFFFFFFF")
 
-    mock_file_manager.write_nand_txt.assert_called()
+    ssd.file_manager.write_nand_txt.assert_called()
 
 
-def test_write_check_file(mocker: MockerFixture):
-    mock_file_manager = mocker.Mock(spec=FileManager)
-    ssd = SSD(mock_file_manager)
+def test_write_check_file(ssd):
     loc = 11
     ssd.write(loc, "0x1298CDEF")
 
-    mock_file_manager.write_nand_txt.assert_called()
+    ssd.file_manager.write_nand_txt.assert_called()
 
 
-def test_out_of_index_error(mocker: MockerFixture):
-    mock_file_manager = mocker.Mock(spec=FileManager)
-    ssd = SSD(mock_file_manager)
+def test_out_of_index_error(ssd):
     loc = 120
     ssd.write(loc, "0x1298CDEF")
 
-    mock_file_manager.write_nand_txt.assert_not_called()
-    
+    ssd.file_manager.write_nand_txt.assert_not_called()
