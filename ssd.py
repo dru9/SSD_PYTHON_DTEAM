ed949ea14cebbfa3760a833d55b2480a877f7494
ldy <ldy@example.com>
2025-07-16 11:16:03 +0900

    [HOTFIX] filelock 빼기 erase size + size > 100으로 변경

--- ssd.py
+++ ssd.py
@@ -1,43 +1,35 @@
 import os
 import sys
-
-from filelock import Timeout, FileLock
-
-from constant import FILENAME, FILENAME_OUT, FILENAME_LOCK, FILENAME_OUT_LOCK
+from constant import FILENAME, FILENAME_OUT
 
 
 class FileManager:
     def __init__(self):
-        self.filename_lock = FileLock(FILENAME_LOCK, timeout=10)
-        self.filename_out_lock = FileLock(FILENAME_OUT_LOCK, timeout=10)
         self.init_nand_txt()
 
     def init_nand_txt(self):
         if not os.path.exists(FILENAME):
-            with self.filename_lock:
-                with open(FILENAME, "w") as f:
-                    for i in range(100):
-                        f.write(f"{i}\t0x00000000\n")
+            with open(FILENAME, "w") as f:
+                for i in range(100):
+                    f.write(f"{i}\t0x00000000\n")
 
     def _read_whole_contents_nand_txt(self) -> dict[int, str]:
         result = {}
-        with self.filename_lock:
-            with open(FILENAME, "r") as f:
-                for line in f:
-                    line = line.strip()
-                    if not line:
-                        continue  # 빈 줄 무시
-                    parts = line.split("\t")
-                    if len(parts) != 2:
-                        continue
-                    result[int(parts[0])] = parts[1]
+        with open(FILENAME, "r") as f:
+            for line in f:
+                line = line.strip()
+                if not line:
+                    continue  # 빈 줄 무시
+                parts = line.split("\t")
+                if len(parts) != 2:
+                    continue
+                result[int(parts[0])] = parts[1]
         return result
 
     def _save_to_nand_file(self, data) -> None:
-        with self.filename_lock:
-            with open(FILENAME, "w") as f:
-                for key, value in data.items():
-                    f.write(f"{key}\t{value}\n")
+        with open(FILENAME, "w") as f:
+            for key, value in data.items():
+                f.write(f"{key}\t{value}\n")
 
     def read_nand_txt(self, lba):
         data_list = self._read_whole_contents_nand_txt()
@@ -63,9 +55,8 @@ class FileManager:
         return True
 
     def write_output_txt(self, contents: str):
-        with self.filename_out_lock:
-            with open(FILENAME_OUT, "w") as f:
-                f.write(contents)
+        with open(FILENAME_OUT, "w") as f:
+            f.write(contents)
 
 
 class SSD:
@@ -126,7 +117,7 @@ class SSD:
             self.read(lba)
         elif mode == "E" and argument_len == 4:
             size = self._parse_int_or_empty(args[3])
-            if size == "" or size < 1 or size > 10 or lba + size > 99:
+            if size == "" or size < 1 or size > 10 or lba + size > 100:
                 self.file_manager.write_output_txt("ERROR")
                 print("Invalid argument")
             else:
